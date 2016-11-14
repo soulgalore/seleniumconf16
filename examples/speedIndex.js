4f1411245d36a3e0bf81d1237d894a2b71b12ad3
soulgalore <peter@soulgalore.com>
2016-11-14 10:52:51 +0100

    kind of how to use SpeedIndex

---
+++ examples/speedIndex.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// woho implementing SpeedIndex is a lot of work
+// so look at our implementation in Browsertime
+// https://github.com/sitespeedio/browsertime
+console.log('Checkout https://github.com/sitespeedio/browsertime and search for SpeedIndex and FFMpeg');
